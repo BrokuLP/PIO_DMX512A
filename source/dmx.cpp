00b2b2c3e2f785620237fc93b6e6fbda4951b731
BrokuLP <86626273+BrokuLP@users.noreply.github.com>
2024-05-18 12:48:50 +0200

    started implementation of core functions

---
+++ source/dmx.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file dmx.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-05-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
---
+++ source/dmx.h
@@ -0,0 +1,29 @@
+/**
+ * @file dmx.h
+ * @author your name (you@domain.com)
+ * @brief header file of dmx library this file inmplements core functions of the dmx library that are shared between the receiver and controller
+ * @version 0.1
+ * @date 2024-05-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+
+class dmx
+{
+private:
+    void _sendFrame();
+public:
+    dmx(/* args */);
+    ~dmx();
+};
+
+dmx::dmx(/* args */)
+{
+}
+
+dmx::~dmx()
+{
+}
